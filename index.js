0a0a0096bd09c184951be316ccb35eb6b70d4496
rholdorp <ralph.holdorp@gmail.com>
2022-06-09 08:09:40 +0200

    reading s3 object -> appending write s3 object

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 
-const upload = async () => {
+const upload = async (originalData) => {
    const params = {
        ACL: 'public-read',
-       Body: 'Hey Ralph en Frank',
+       Body: originalData + 'Hey Ralph en Frank',
        ContentType: 'text/html',
        Bucket: 'ndbrgrd-ws-data',
        Key: 'testfile.txt'
@@ -19,8 +19,26 @@ const upload = async () => {
    });
 }
 
+const read = async () => {
+    const params = {
+        ACL: 'public-read',
+        Bucket: 'ndbrgrd-ws-data',
+        Key: 'testfile.txt'
+    } 
+     return await new Promise((resolve, reject) => {
+         s3.getObject(params, (err, results) => {
+            if(err) reject(err);
+            else resolve(results);             
+         });
+    });
+}
+
 const main = async(event) => {
-    return upload();
+    var r = await read().then((e,r) =>{
+        console.log("error,result="+ e,r);
+    });
+    console.log(r);
+    return upload(r);
 };
 
 exports.handler = main;
